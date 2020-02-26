51b5d90a75fb39482cb3e80f17aae28e3b2bab67
lchua2314 <lchua2314@gmail.com>
2020-02-25 22:35:43 -0800

    first commit

---
+++ fp.java
@@ -0,0 +1,58 @@
+// fp.java
+
+/**
+ * WRIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIITE DESSSSSSSSSSSSSSSSSC
+ * @author Lawrence Chua
+ */
+public class fp
+{
+	// Be sure to put your name on this next line...
+        /**
+         * Returns programmer's name
+         * @return Programmer's name
+         */
+	public String myName()
+	{
+		return "Lawrence Chua";
+	}
+
+	public int add(int a, int b)
+	{
+		FPNumber fa = new FPNumber(a);
+		FPNumber fb = new FPNumber(b);
+		FPNumber result = new FPNumber(0);
+
+		// Put your code in here!
+
+		return result.asInt();
+	}
+
+	public int mul(int a, int b)
+	{
+		FPNumber fa = new FPNumber(a);
+		FPNumber fb = new FPNumber(b);
+		FPNumber result = new FPNumber(0);
+
+		// Put your code in here!
+
+		return result.asInt();
+	}
+
+	// Here is some test code that one student had written...
+	public static void main(String[] args)
+	{
+		int v24_25	= 0x41C20000; // 24.25
+		int v_1875	= 0xBE400000; // -0.1875
+		int v5		= 0xC0A00000; // -5.0
+
+		fp m = new fp();
+
+		System.out.println(Float.intBitsToFloat(m.add(v24_25, v_1875)) + " should be 24.0625");
+		System.out.println(Float.intBitsToFloat(m.add(v24_25, v5)) + " should be 19.25");
+		System.out.println(Float.intBitsToFloat(m.add(v_1875, v5)) + " should be -5.1875");
+
+		System.out.println(Float.intBitsToFloat(m.mul(v24_25, v_1875)) + " should be -4.546875");
+		System.out.println(Float.intBitsToFloat(m.mul(v24_25, v5)) + " should be -121.25");
+		System.out.println(Float.intBitsToFloat(m.mul(v_1875, v5)) + " should be 0.9375");
+	}
+}
